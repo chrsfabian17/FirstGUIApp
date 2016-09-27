3f790533aaac9b2eafc30f027b1dcba91840c195
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-27 08:37:12 -0600

    Created a return trype
    I created a turn type as well as discarded bin/, xcuserdata, & .DS_Store

--- src/popups/view/PopupViewer.java
+++ src/popups/view/PopupViewer.java
@@ -1,6 +1,30 @@
 package popups.view;
 
+import javax.swing.JOptionPane;
+
 public class PopupViewer
 {
 
+	/**
+	 * makes a popup window to display the supplied message.
+	 * @param message The message to be placed in the popup.
+	 * 
+	 */
+	public void displayMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
+	/**
+	 * Creates an input popup to collect a response as a string
+	 * @param question The question being asked to the user
+	 * @return The users response. 
+	 */
+	public String collectResponse(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
 }
