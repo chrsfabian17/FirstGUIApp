e6c7d540eb551b266ebc75c4fd3a28393b2dfaca
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-29 08:42:00 -0600

    created a second try catch statement
    This one is different it is an integer instead a boolean.

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -24,26 +24,48 @@ public class PopupsController
 		}
 	}
 	/**
-	 * Checks is the supplied String can be parseed to a double value.
+	 * Checks is the supplied String can be parsed to a double value.
 	 * @param potentialValue The string to test.
 	 * @return Weather the value could be parsed; true if it can be parsed; false otherwise.
 	 */
 	private boolean isDouble(String potentialValue)
 	{
-		boolean isParsable = false;
+		boolean isParseable = false;
 		
 		try
 		{
 			double test = Double.parseDouble(potentialValue);
-			isParsable = true;
+			isParseable = true;
 		}
 		catch(NumberFormatException notDouble)
 		{
 			display.displayMessage("This not a double value =:((");
 		}
 		
-		return isParsable;
+		return isParseable;
 	
+	}
+	/**
+	 * Checks if the supplied String can be Parsed to a integer value.
+	 * @param potentialNumber The String to test.
+	 * @returnWeather the number could be parsed; true if it can be parsed; false if otherwise.
+	 */
+	private boolean isInteger(String potentialNumber)
+	{
+		boolean isParseable = false;
+		
+		try
+		{
+			int test = Integer.parseInt(potentialNumber);
+			isParseable = true;
+		}
+		catch(NumberFormatException ontInt)
+		{
+		display.displayMessage(notInt.getMessage());
+		display.displayMessage("Type in an integer next time!!!");	
+		}
+		
+		return isParseable;
 	}
 	
 	
