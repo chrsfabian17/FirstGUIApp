117267e5b2ba05dceaf2ced1f89d03c00a2e8a89
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-29 08:25:53 -0600

    Created try and catch methods

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -21,7 +21,30 @@ public class PopupsController
 		
 		answer = display.collectResponse("What the heck is going on?!");
 		display.displayMessage(answer + " is what you said");
-		
 		}
 	}
+	/**
+	 * Checks is the supplied String can be parseed to a double value.
+	 * @param potentialValue The string to test.
+	 * @return Weather the value could be parsed; true if it can be parsed; false otherwise.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isParsable = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isParsable = true;
+		}
+		catch(NumberFormatException notDouble)
+		{
+			display.displayMessage("This not a double value =:((");
+		}
+		
+		return isParsable;
+	
+	}
+	
+	
 }
